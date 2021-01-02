944674233a76b726e6ed91575eacdeb5ceaf98df
starrier <starrier@starrier.org>
2021-01-02 20:00:15 +0800

    add unit test

---
+++ src/main/test/org/starrier/common/validation/IsMobileValidatorTest.java
@@ -0,0 +1,23 @@
+package org.starrier.common.validation;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * @author starrier
+ * @date 2021/1/2
+ */
+class IsMobileValidatorTest {
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    @DisplayName("mobilePhone param validate")
+    void isValid() {
+
+        String  mobilePhone = "12234567889";
+    }
+}
