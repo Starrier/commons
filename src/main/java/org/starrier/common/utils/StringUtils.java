2e4208547cbde2dda6ebceaf93484b20513dfbb8
starrier <starrier@starrier.org>
2021-03-16 16:16:10 +0800

    :unicorn: upgrade aes-utils

---
+++ src/main/java/org/starrier/common/utils/StringUtils.java
@@ -0,0 +1,31 @@
+package org.starrier.common.utils;
+
+/**
+ * @author starrier
+ * @date 2021/3/15
+ */
+public class StringUtils {
+
+    public static boolean isBlank(CharSequence cs) {
+        int strLen = length(cs);
+        if (strLen == 0) {
+            return true;
+        } else {
+            for(int i = 0; i < strLen; ++i) {
+                if (!Character.isWhitespace(cs.charAt(i))) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    public static int length(CharSequence cs) {
+        return cs == null ? 0 : cs.length();
+    }
+
+    public static boolean isNotBlank(CharSequence cs) {
+        return !isBlank(cs);
+    }
+}
